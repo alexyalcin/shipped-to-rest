2c4390b3db7a504c27e2b64fafb1dea7609d821b
Alexander Yalcin <ayalcin@elon.edu>
2018-10-28 21:20:04 -0400

    Adding Project

---
+++ Shipped to Rest/Assets/Scripts/FPSInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public float speed = 6.0f;
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+        0,
+        Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+
+}
